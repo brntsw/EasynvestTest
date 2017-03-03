06c6fec32597b771894e23430b3b883b781c89ed
brntsw <bruno.pardini6@gmail.com>
2017-03-03 20:58:46 -0300

    Adicionada a estrutura inicial do app, contendo algumas das libraries a serem utilizadas no app

---
+++ app/src/main/java/easynvest/bruno/com/easynvesttest/ui/activity/FormularioActivity.java
@@ -0,0 +1,15 @@
+package easynvest.bruno.com.easynvesttest.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import easynvest.bruno.com.easynvesttest.R;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
